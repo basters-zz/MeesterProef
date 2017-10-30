476f8c0c608ed7848f66ef4af0e71d8cf5c35f77
BAS\bas <bas.1997@hotmail.com>
2017-10-30 13:50:07 +0100

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+	private Vector3 tmp;
+	float cycleMins;
+	float cycleCalc;
+	Transform directionalLight;
+	//public bool enemySpawn;
+	GameObject score;
+	GameObject sheeps;
+	GameObject wolves;
+	GameObject days;
+	GameObject DirectionalLight;
+	GameObject mainCam;
+	int e;
+	int[] sheeps1;
+	bool shiftPressed;
+	List<GameObject> sheeplist = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		directionalLight = GameObject.FindGameObjectWithTag ("Sun").transform;
+		cycleMins = 10;
+		cycleCalc = 0.1f / cycleMins * 1;
+
+		sheeps = GameObject.Find ("Sheeps");
+		score = GameObject.Find ("Score");
+		wolves = GameObject.Find ("Wolves");
+		days = GameObject.Find ("Days");
+		DirectionalLight = GameObject.FindGameObjectWithTag ("Sun");
+		mainCam = GameObject.FindGameObjectWithTag ("MainCamera");
+		shiftPressed = false;
+		sheeplist.AddRange (GameObject.FindGameObjectsWithTag ("Sheep"));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		CameraController ();
+		CheckSheep ();
+		CheckWolves ();
+		DayNightCycle ();
+
+		if(Input.GetKeyDown(KeyCode.R)){
+			KillSheep ();
+		}
+		Debug.Log (sheeplist.Count);
+		/*foreach(GameObject sheep in sheeplist){
+			sheep.GetComponent<SheepController>().
+		}*/
+	}
+
+	void DayNightCycle(){
+		directionalLight.Rotate(cycleCalc, 0, 0);
+	}
+
+	void CheckSheep(){
+		sheeps.GetComponent<InputField> ().text = "Sheep: " + GameObject.FindGameObjectsWithTag ("Sheep").Length.ToString ();
+
+	}
+	void CheckWolves(){
+		wolves.GetComponent<InputField> ().text = "Wolves: " + GameObject.FindGameObjectsWithTag ("Wolf").Length.ToString ();
+	}
+	void KillSheep(){
+//		sheeps1 = GameObject.FindGameObjectsWithTag ("Sheep").Length;
+		foreach(GameObject sheep in sheeplist){
+			e = e + Random.Range (0, 3);
+			Debug.Log (e);
+		}
+		Debug.Log ("e: " + e);
+	}
+
+	void CameraController(){
+		Vector3 tmpXZ = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		Vector3 tmpY = new Vector3 (mainCam.transform.position.x, mainCam.transform.position.y, mainCam.transform.position.z);
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			shiftPressed = true;
+		}
+		else{
+			shiftPressed = false;
+		}
+		if (Input.GetKey (KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+			if (!shiftPressed) {
+				tmpXZ.z += 0.1f;
+				transform.position = tmpXZ;
+			} else {
+				tmpXZ.z += 0.3f;
+				transform.position = tmpXZ;
+			}
+		}
+		if (Input.GetKey (KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+			if (!shiftPressed) {
+				tmpXZ.x -= 0.1f;
+				transform.position = tmpXZ;
+			} else {
+				tmpXZ.x -= 0.3f;
+				transform.position = tmpXZ;
+			}
+		}
+		if (Input.GetKey (KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+			if (!shiftPressed) {
+				tmpXZ.x += 0.1f;
+				transform.position = tmpXZ;
+			} else {
+				tmpXZ.x += 0.3f;
+				transform.position = tmpXZ;
+			}
+		}
+		if (Input.GetKey (KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+			if (!shiftPressed) {
+				tmpXZ.z -= 0.1f;
+				transform.position = tmpXZ;
+			} else {
+				tmpXZ.z -= 0.3f;
+				transform.position = tmpXZ;
+			}
+		}
+
+		if(Input.GetAxis("Mouse ScrollWheel") < 0){
+			tmpY.y++;
+			mainCam.transform.position = tmpY;
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/SheepController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour {
+	private float speed; //movement speed
+	private Vector3 transformZ;
+	private bool isWalking;
+	private bool isEating;
+	private Animator anim;
+	private float eatTimer;
+	private float deadSheepTimer;
+	public GameObject explosion;
+	static bool deadSheep;
+	// Use this for initialization
+	void Start () {
+		speed = 1.5f; //declare the actual speed
+		isWalking = true;
+		anim = GetComponent<Animator> ();
+		eatTimer = Random.Range (10, 25);
+		deadSheep = false;
+		deadSheepTimer = 0.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isWalking) {
+			Walking ();
+			eatTimer -= Time.deltaTime;
+		} else {
+			anim.SetBool ("IsWalking", false);
+		}
+		if(eatTimer <= 0){
+			isWalking = false;
+			isEating = true;
+			if (isEating) {
+				StartCoroutine (Eating());
+			}
+		}
+		if(deadSheep == true){
+			deadSheepTimer -= Time.deltaTime;
+			if(deadSheepTimer <= 0){
+				deadSheepTimer = 0.5f;
+				deadSheep = false;
+			}
+		}
+
+	}
+	void Walking(){
+		transform.Translate (transformZ = new Vector3 (0, 0, 2) * speed * Time.deltaTime);
+		anim.SetBool("IsWalking", true);
+		Vector3 fwd = transform.TransformDirection (Vector3.forward);
+		RaycastHit hit;
+
+		Debug.DrawRay (transform.position, fwd * 3, Color.red);
+		if (Physics.Raycast (transform.position, fwd, out hit, 3)) {
+			if (hit.collider.tag == "Walls" || hit.collider.tag == "Sheep") {
+				int randomint = Random.Range (0, 360);
+				transform.Rotate (0, randomint, 0);
+			}
+		}
+	}
+	IEnumerator Eating(){
+		anim.SetBool("IsEating", true);
+		yield return new WaitForSecondsRealtime (5f);
+		anim.SetBool("IsEating", false);
+		isWalking = true;
+		eatTimer = Random.Range (10, 32);
+	}
+	void OnMouseDown(){
+		Explode ();
+	}
+	public void Explode(){
+
+		Instantiate(explosion, transform.position, transform.rotation);
+		Destroy(gameObject);
+		deadSheep = true;
+	}
+}
